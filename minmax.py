68ed80f5fdbeeb1d894a3ef0217fe37971877997
David Kossia <davidkossia@utexas.edu>
2024-02-17 00:34:22 -0600

    fixed spacing

--- minmax.py
+++ minmax.py
@@ -8,15 +8,12 @@ if inp == 'stop':   # no numbers entered
     print()
 else:
     min, max = inp, inp  # set min and max initially to first input
-
     while inp != 'stop':
         count += 1
-
         if inp > max:   # new max
             max = inp
         if inp < min:   # new min
             inp = min
-
         inp = input("Enter an integer or 'stop' to end:").strip()  # next input
 
     print()
